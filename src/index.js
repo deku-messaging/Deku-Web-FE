e319f9cbfd5eb00ddcb9c8f1781c95655c7c87c5
Minion <nessakristo@gmail.com>
2023-06-27 17:38:40 +0100

    creating auth page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import CssBaseline from "@mui/material/CssBaseline";
+import Auth from "./Authenticate";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
